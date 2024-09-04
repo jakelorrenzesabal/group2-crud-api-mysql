a9603e7e7ff6ee808de8009139f15a5f87a15783
jakelorenzesabal <jakelorenzecan@gmail.com>
2024-09-04 21:12:46 +0800

    query search all data by letter oo full

--- users/user.service.js
+++ users/user.service.js
@@ -1,10 +1,10 @@
 const bcrypt = require('bcryptjs');
 const db = require('_helpers/db');
+const { Op } = require('sequelize');
 
 module.exports = {
     getAll,
     getById,
-    
     create,
     update,
     delete: _delete,
@@ -14,47 +14,12 @@ module.exports = {
 async function getAll() {
     return await db.User.findAll();
 }
+
 async function getById(id) {
     return await getUser(id);
 } 
-async function search(query) {
-    // Construct search conditions with `LIKE` for partial matches
-    const where = {};
 
-    if (query.email) {
-        where.email = { [Sequelize.Op.iLike]: `%${query.email}%` }; // Case-insensitive match
-    }
-    if (query.firstName) {
-        where.firstName = { [Sequelize.Op.iLike]: `%${query.firstName}%` }; // Case-insensitive match
-    }
-    if (query.lastName) {
-        where.lastName = { [Sequelize.Op.iLike]: `%${query.lastName}%` }; // Case-insensitive match
-    }
-    if (query.title) {
-        where.title = { [Sequelize.Op.iLike]: `%${query.title}%` }; // Case-insensitive match
-    }
-    if (query.role) {
-        where.role = { [Sequelize.Op.iLike]: `%${query.role}%` }; // Case-insensitive match
-    }
-    if (query.profilePic) {
-        where.profilePic = { [Sequelize.Op.iLike]: `%${query.profilePic}%` }; // Case-insensitive match
-    }
-
-    try {
-        console.log('Search criteria:', where); // Debugging statement
-
-        // Find all users that match the search criteria
-        const users = await db.User.findAll({ where });
-        console.log('Search results:', users); // Debugging statement
-
-        return users;
-    } catch (error) {
-        console.error('Error:', error); // Debugging statement
-        throw error;
-    }
-}
 async function create(params) {
-    
     if (await db.User.findOne({ where: { email: params.email } })) {
         throw 'Email "' + params.email + '" is already registered';
     }
@@ -63,6 +28,7 @@ async function create(params) {
     user.passwordHash = await bcrypt.hash(params.password, 10);
     await user.save();
 }
+
 async function update(id, params) {
     const user = await getUser(id);
 
@@ -71,18 +37,40 @@ async function update(id, params) {
         throw 'Username "' + params.username + '" is already taken';
     }
     
-    if (params.password) 
+    if (params.password) {
         params.passwordHash = await bcrypt.hash(params.password, 10);
+    }
     
     Object.assign(user, params);
     await user.save();
 }
+
 async function _delete(id) {
     const user = await getUser(id);
     await user.destroy();
 }
+
 async function getUser(id) {
     const user = await db.User.findByPk(id);
-    if (!user) throw 'User not found';
+    if (!user) throw 'User ad found';
     return user;
+}
+
+
+async function search(query) {
+    // Perform a case-insensitive search across multiple fields
+    const users = await db.User.findAll({
+        where: {
+            [Op.or]: [
+                { email: { [Op.like]: `%${query}%` } },
+                { title: { [Op.like]: `%${query}%` } },
+                { firstName: { [Op.like]: `%${query}%` } },
+                { lastName: { [Op.like]: `%${query}%` } },
+                { role: { [Op.like]: `%${query}%` } }
+            ]
+        }
+    });
+
+    if (users.length === 0) throw new Error('No users found matching the search criteria');
+    return users;
 }
