3ae592056cda844012a18cf6abc9e08cbf99fe07
jake rojas <jakaerojas2211@gmail.com>
2024-09-08 13:55:43 +0800

    Adding Preferences and Settings function

--- users/user.model.js
+++ users/user.model.js
@@ -4,13 +4,20 @@ module.exports = model;
 
 function model(sequelize) {
     const attributes = {
+        //======For Profile=================
         email: { type: DataTypes.STRING, allowNull: false },
         passwordHash: { type: DataTypes.STRING, allowNull: false },
         title: { type: DataTypes.STRING, allowNull: false },
         firstName: { type: DataTypes.STRING, allowNull: false },
         lastName: { type: DataTypes.STRING, allowNull: false },
         role: { type: DataTypes.STRING, allowNull: false },
-        profilePic: { type: DataTypes.STRING, allowNull: false }
+        profilePic: { type: DataTypes.STRING, allowNull: false },
+
+        //======For Preferences=================
+        theme: { type: DataTypes.STRING, allowNull: true, defaultValue: 'light' },
+        notifications: { type: DataTypes.BOOLEAN, allowNull: true, defaultValue: true },
+        language: { type: DataTypes.STRING, allowNull: true, defaultValue: 'en' },
+
     };
     
     const options = {
